a082222b3ae9170b690bc5f668adf5cfdb150252
TheCrafter <dimitrisvlh@gmail.com>
2021-04-24 15:50:13 +0300

    Added camera controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // TODO: Check this out -> https://forum.unity.com/threads/rts-camera-script.72045/
+
+    [SerializeField] float panSpeed = 30f;
+    [SerializeField] float scrollSpeed = 5f;
+    [SerializeField] float panBorderThickness = 10f;
+    [SerializeField] float minY = 10f;
+    [SerializeField] float maxY = 80f;
+
+    private bool doMovement = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+
+        // Disable movement
+        if (!doMovement) { return; }
+
+        // Handle movement input
+        Vector3 moveVector = Vector3.zero;
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            moveVector = Vector3.forward;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            moveVector = Vector3.back;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            moveVector = Vector3.right;
+        } 
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            moveVector = Vector3.left;
+        }
+
+        // Move
+        transform.Translate(moveVector * panSpeed * Time.deltaTime, Space.World);
+
+        // Scrolling
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+
+    }
+}
