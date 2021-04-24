f04f5caf6bafd9c28d4737dd41ea9498258e96b4
TheCrafter <dimitrisvlh@gmail.com>
2021-04-25 01:30:12 +0300

    Added missiles to missile launcher

--- Assets/Tower/Bullet.cs
+++ Assets/Tower/Bullet.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed = 70f;
+    [SerializeField] float explosionRadius = 0f;
     [SerializeField] GameObject impactEffect;
 
     private Transform target;
@@ -31,19 +32,51 @@ public class Bullet : MonoBehaviour
 
         // Move
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     private void HitTarget()
     {
         GameObject effects = Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effects, 2f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
-        Destroy(target.gameObject); // TODO: Remove
-        target = null;
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
     }
 
     public void Seek(Transform seekTarget)
     {
         target = seekTarget;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
